e40f094b3ff3986a0bace001e47d71835af04918
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-03-29 12:25:11 -0700

    Password Generator

---
+++ README.md
@@ -0,0 +1,29 @@
+# Password Generator Starter Code
+
+Direct Link:
+======
+https://pops08.github.io/Joshua-H-Password-Generator/
+
+Author:
+======
+Joshua H. (Pops08)
+
+
+User Story:
+======
+AS AN employee with access to sensitive data
+
+I WANT to randomly generate a password that meets certain criteria
+
+SO THAT I can create a strong password that provides greater security
+
+
+Description:
+======
+For the Password Generator assignment, I've created my page using HTML, CSS, Javascript, and Github to prompt the user for length amount, to include upper or lower case characters, numbers, and/or special characters
+
+
+After Image:
+======
+
+![After Image](Final.png)
---
+++ script.js
@@ -0,0 +1,80 @@
+// Assignment code here
+var increment = 0;
+
+var Upper = "ABCDEFGHIJKLMNOP";
+var Lower = Upper.toLowerCase();
+var Special = "'!@#$%^&*()']";
+var Numbers = "0123456789";
+
+var password = '';
+var finalPass = '';
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Generates the Password
+function generatePassword() {
+
+  var passLength = window.prompt("Please Enter A Number Between 8 - 128 For Your Password");
+  passLength = parseInt(passLength);
+
+  while (passLength < 8 || passLength > 128){
+    window.alert("The Number You Have Selected Is NOT Within The Range");
+    var passLength = window.prompt("Please Enter A Number Between 8 - 128 For Your Password");
+    passLength = parseInt(passLength);
+  }
+
+  var passUpper = window.confirm("Include Upper Case?");
+  var passSpecial = window.confirm("Include Special Characters?");
+  var passLower = window.confirm("Include Lower Case?");
+  var passNumber = window.confirm("Include Numbers?");
+
+  const object = {
+    passUpper: Upper,
+    passSpecial: Special,
+    passLower: Lower,
+    passNumber: Numbers
+  };
+
+  for (var [key, value] of Object.entries(object)) {
+
+    debugger;
+
+    if (passUpper) {
+      password += Upper;
+      passUpper = '';
+    } else if (passSpecial) {
+      password += Special;
+      passSpecial = '';
+    } else if (passLower) {
+      password += Lower;
+      passLower = '';
+    } else if (passNumber) {
+      password += Numbers;
+      passNumber = '';
+    }
+
+  }
+
+  console.log("Password: " + password);
+
+  while (increment < passLength) {
+    finalPass += password[Math.floor(Math.random() * password.length)];
+    increment++;
+  }
+
+  return finalPass;
+}
